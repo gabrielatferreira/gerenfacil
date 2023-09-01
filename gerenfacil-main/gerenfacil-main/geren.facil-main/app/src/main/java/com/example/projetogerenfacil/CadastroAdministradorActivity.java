73c4f24bbd8ddb1f83860d00d3605b1e4f74080d
Leonardo Xavier <leonardo.xavier@acad.ftec.com.br>
2023-08-31 21:40:46 -0300

    Adicionado cadastro usuario e administrador

---
+++ gerenfacil-main/gerenfacil-main/geren.facil-main/app/src/main/java/com/example/projetogerenfacil/CadastroAdministradorActivity.java
@@ -0,0 +1,21 @@
+package com.example.projetogerenfacil;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CadastroAdministradorActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastro_administrador);
+
+
+
+    }
+}
---
+++ gerenfacil-main/gerenfacil-main/geren.facil-main/app/src/main/java/com/example/projetogerenfacil/CadastroUsuarioActivity.java
@@ -0,0 +1,18 @@
+package com.example.projetogerenfacil;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CadastroUsuarioActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastro_usuario);
+
+
+
+    }
+}
