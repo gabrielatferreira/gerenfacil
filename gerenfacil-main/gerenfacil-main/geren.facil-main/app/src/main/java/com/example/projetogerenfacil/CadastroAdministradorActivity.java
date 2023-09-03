cd5c240e0a5414c5b7859fbe1453c95314793866
Anderson <86ander@gmail.com>
2023-09-03 11:53:36 -0300

    criado clicavel Faça login

--- gerenfacil-main/gerenfacil-main/geren.facil-main/app/src/main/java/com/example/projetogerenfacil/CadastroAdministradorActivity.java
+++ gerenfacil-main/gerenfacil-main/geren.facil-main/app/src/main/java/com/example/projetogerenfacil/CadastroAdministradorActivity.java
@@ -3,19 +3,25 @@ package com.example.projetogerenfacil;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CadastroAdministradorActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cadastro_administrador);
 
-
-
+        TextView tvLogin = findViewById(R.id.tvLogin);
+        tvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Redirecione para a tela de login (MainActivity) quando "Faça login" for clicado
+                Intent intent = new Intent(CadastroAdministradorActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- gerenfacil-main/gerenfacil-main/geren.facil-main/app/src/main/java/com/example/projetogerenfacil/CadastroUsuarioActivity.java
+++ gerenfacil-main/gerenfacil-main/geren.facil-main/app/src/main/java/com/example/projetogerenfacil/CadastroUsuarioActivity.java
@@ -1,18 +1,27 @@
 package com.example.projetogerenfacil;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CadastroUsuarioActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cadastro_usuario);
 
-
-
+        TextView tvLogin = findViewById(R.id.tvLogin);
+        tvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Redirecione para a tela de login (MainActivity) quando "Faça login" for clicado
+                Intent intent = new Intent(CadastroUsuarioActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
